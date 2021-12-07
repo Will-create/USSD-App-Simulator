593707e5b4baa20091f28e0f96eebf6879ad711e
Louis Bertson <louisbertson@gmail.com>
2021-12-07 10:27:30 +0000

    Les autres ne sont jamais les memes

--- libs/js/form-modal.js
+++ libs/js/form-modal.js
@@ -1,36 +1,40 @@
-let modal = document.querySelector("#shortCodeInfoModal");
-let modalOverlay = document.querySelector("#modal-overlay");
-let closeButton = document.querySelector("#close-button");
-let openButton = document.querySelector("#open-button");
+// let modal = document.querySelector("#shortCodeInfoModal");
+// let modalOverlay = document.querySelector("#modal-overlay");
+// // let closeButton = document.querySelector("#close-button");
+// let openButton = document.querySelector("#open-button");backendUrl
 let paramsBtn = document.querySelector("#paramsBtn");
 let backBtn = document.querySelector("#backBtn");
 let paramsForm = document.querySelector("#paramsForm");
 let styleForm = document.querySelector("#styleForm");
-const envForm = document.querySelector("#env-form")
-const envFormModalClose = document.querySelector('#form-modal-close')
+let backendUrl = document.querySelector("#backendUrl");
+// const envForm = document.querySelector("#env-form")
+// const envFormModalClose = document.querySelector('#form-modal-close')
 const registrationForm = document.querySelector("#shortCodeInformationForm");
 
 registrationForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    alert("Form submitted");
+console.log(e);
+localStorage.clear();
+APPLICATION_URL =backendUrl.value;
+console.log(APPLICATION_URL);
 });
 
-closeButton.addEventListener("click", function () {
-    modal.classList.toggle("closed");
-    modalOverlay.classList.toggle("closed");
-});
+// closeButton.addEventListener("click", function () {
+//     modal.classList.toggle("closed");
+//     modalOverlay.classList.toggle("closed");
+// });
 
 
-envFormModalClose.addEventListener('click', () => {
-    envForm.classList.toggle('form_visible')
-})
+// envFormModalClose.addEventListener('click', () => {
+//     envForm.classList.toggle('form_visible')
+// })
 paramsBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    paramsForm.classList.toggle('invisible');
-    styleForm.classList.toggle('invisible');
+    paramsForm.css('display','block');
+    styleForm.css('display','none');
 })
 backBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    paramsForm.classList.toggle('invisible');
-    styleForm.classList.toggle('invisible');
+    paramsForm.css('display','none');
+    styleForm.css('display','block');
 })
